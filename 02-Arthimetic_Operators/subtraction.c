21b65eadc535d9824cb65af594abd36a0d3d42d2
Ankit Baheti <ankitbaheti373@gmail.com>
2026-09-17 20:35:18 +0530

    Fix output message for subtraction result

--- 02-Arthimetic_Operators/subtraction.c
+++ 02-Arthimetic_Operators/subtraction.c
@@ -9,6 +9,6 @@ int main(){
     printf("Enter your second number : ");
     scanf("%d", &num2);
     sub = num1 - num2;
-    printf("The addition of given two numbers is = %d", sub);
+    printf("The difference of given two numbers is = %d", sub);
     return 0;
 }
