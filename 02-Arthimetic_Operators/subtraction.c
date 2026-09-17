aabf9eaf873d685a25a314a9d28cc1fff5e340c5
Ankit Baheti <ankitbaheti373@gmail.com>
2026-09-17 20:34:06 +0530

    Add program subtract two integers

---
+++ 02-Arthimetic_Operators/subtraction.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int num1;
+    int num2;
+    int sub;
+    printf("Enter your first number : ");
+    scanf("%d", &num1);
+    printf("Enter your second number : ");
+    scanf("%d", &num2);
+    sub = num1 - num2;
+    printf("The addition of given two numbers is = %d", sub);
+    return 0;
+}
