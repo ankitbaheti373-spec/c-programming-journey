fb5f1f2259b35d9f04fd35c4274d77479fd05997
Ankit Baheti <ankitbaheti373@gmail.com>
2026-09-17 20:37:52 +0530

    Subtract two integers

--- 02-Arthimetic_Operators/subtraction.c
+++ 02-Arthimetic_Operators/subtraction.c
@@ -7,7 +7,7 @@ int main(){
     printf("Enter your first number : ");
     scanf("%d", &num1);
     printf("Enter your second number : ");
-    scanf("%d", &num2);
+    scanf("%d", &num2); 
     sub = num1 - num2;
     printf("The difference of given two numbers is = %d", sub);
     return 0;
