5e669826fa91a8c8924dcd0f79a81a0c17added1
Ankit Baheti <ankitbaheti373@gmail.com>
2026-09-18 21:08:14 +0530

    Updated problem-04

--- 40 basic coding problems/problem-04.c
+++ 40 basic coding problems/problem-04.c
@@ -1,2 +1,18 @@
 // Take two integers and print their sum.
 #include<stdio.h>
+
+int main(){
+
+    int num1;
+    int num2;
+    int sum;
+    printf("Enter your first number : ");
+    scanf("%d", &num1);
+    printf("Enter your second number : ");
+    scanf("%d", &num2);
+    sum = num1 + num2;
+    printf("The sum of given two numbers is = %d", sum);
+
+    return 0;
+    
+}
