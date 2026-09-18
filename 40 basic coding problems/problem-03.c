0ad42b91b54f0f6e7be34f0743394c4db0dda66b
Ankit Baheti <ankitbaheti373@gmail.com>
2026-09-18 21:02:29 +0530

    Updated problem 3

---
+++ 40 basic coding problems/problem-03.c
@@ -0,0 +1,17 @@
+// Take two integers as input and print both numbers
+#include<stdio.h>
+
+int main(){
+
+    int num1;
+    int num2;
+    printf("Enter your first number : ");
+    scanf("%d", num1);
+    printf("Enter your second number : ");
+    scanf("%d", num2);
+    printf("\nThe first number is  = %d", num1);
+    printf("\nThe second number is = %d", num2);
+
+    return 0;
+    
+}
